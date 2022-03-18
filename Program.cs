bdbc8dda53ea81be856f6f0f0f715611c01dd17c
jalakhras <jalakhras@outlook.com>
2022-03-19 01:20:16 +0300

    InspectingMetadata

--- Program.cs
+++ Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.Reflection;
 
-namespace ReflectionSampel
+namespace ReflectionSample
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+
+        public  void InspectingMetadata()
+        {
+            string name = "jassar";
+            //var stringType = name.GetType();
+            var stringType = typeof(string);
+            Console.WriteLine(stringType);
+
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            var typesFromCurrentAssembly = currentAssembly.GetTypes();
+            foreach (var type in typesFromCurrentAssembly)
+            {
+                Console.WriteLine(type.Name);
+            }
+
+            var oneTypeFromCurrentAssembly = currentAssembly.GetType("ReflectionSample.Person");
+            Console.WriteLine(oneTypeFromCurrentAssembly.Name);
+
+            var externalAssembly = Assembly.Load("System.Text.Json");
+            var typesFromExternalAssembly = externalAssembly.GetTypes();
+            var oneTypeFromExternalAssembly = externalAssembly.GetType("System.Text.Json.JsonProperty");
+
+            var modulesFromExternalAssembly = externalAssembly.GetModules();
+            var oneModuleFromExternalAssembly = externalAssembly.GetModule("System.Text.Json.dll");
+
+            var typesFromModuleFromExternalAssembly = oneModuleFromExternalAssembly.GetTypes();
+            var oneTypeFromModuleFromExternalAssembly =
+                oneModuleFromExternalAssembly.GetType("System.Text.Json.JsonProperty");
+
+            foreach (var constructor in oneTypeFromCurrentAssembly.GetConstructors())
+            {
+                Console.WriteLine(constructor);
+            }
+
+            //foreach (var method in oneTypeFromCurrentAssembly.GetMethods())
+            //{
+            //    Console.WriteLine(method);
+            //}
+
+            foreach (var method in oneTypeFromCurrentAssembly.GetMethods(
+                 BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                Console.WriteLine($"{method}, public: {method.IsPublic}");
+            }
+
+            foreach (var field in oneTypeFromCurrentAssembly.GetFields(
+                BindingFlags.Instance | BindingFlags.NonPublic))
+            {
+                Console.WriteLine(field);
+            }
         }
     }
 }
